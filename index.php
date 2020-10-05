57cfc87c55693e60efa96aafa1a4aab9b8fda0a0
Alex <gurievalex78@gmail.com>
2020-10-05 22:36:03 +0200

    index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'classes/Employee.php';
+require_once 'classes/Manager.php';
+require_once 'classes/Consultant.php';
+
+
+$employee = new Employee('Mario','Rossi','Employee');
+$employee->setCF('ABCDEF12s98992Z1');
+
+var_dump($employee);
+echo $employee->getRole();
+
+
+$manager = new Manager('Pippo','Buono','Manager','No Bonus');
+$priceM = $manager->pricePerHour($manager->getRole());
+
+$manager->setHours(133);
+$hours = $manager->getHours();
+ 
+var_dump($manager);
+echo 'Stipendio: '.$manager->getSalary($priceM, $hours).'$';
